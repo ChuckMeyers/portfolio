ed8317432e6238698ce6cefa58de6775a83758d4
Charles Meyers <cmmeyers23@gmail.com>
2022-12-21 10:15:04 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8000
+
+app.use(cors());
+app.use(express.static(path.join(__dirname + '/public')));
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
